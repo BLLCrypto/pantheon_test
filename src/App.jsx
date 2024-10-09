f252f0ad732fd2fe8fb87107737419eae10818af
BLLCrypto <bllcrypto.neotokyo@gmail.com>
2024-10-08 23:57:11 -0300

    vercel build failed try fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Sidebar from './components/Sidebar'
-import DiagonalBorderCard from './components/test'
 import Logo from "./assets/logo.png"
 import { BorderTop, BorderBottom } from './assets/svg'
 
