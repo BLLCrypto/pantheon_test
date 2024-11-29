12188ff2bae3750999fae127657ef15a51ee094b
BLLCrypto <bllcrypto.neotokyo@gmail.com>
2024-11-29 10:03:42 -0300

    more 2 sections created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -165,10 +165,13 @@ const Home = () => {
                         </div>
                     </div>
                 </section>
+
                 <section className="w-full max-w-7xl flex flex-col justify-center items-center">
-                    <h2 className="text-4xl mb-[24px] mb-[80px]">All-in-One Solution for Your Needs</h2>
+                    <div className="flex items-center mb-[80px]">
+                        <h2 className="text-4xl">All-in-One Solution for Your Needs</h2>
+                    </div>
                     <div className="w-full mb-[60px] flex gap-[32px]">
-                        <div className="w-2/5 bg-beige-light flex flex-col items-center text-center border border-gold-light p-[64px]">
+                        <div className="w-2/5 bg-beige-light flex flex-col items-center text-center border border-gold-light p-[48px]">
                             <p className="text-gold text-xs mb-[12px] tracking-[1.4px] uppercase">Your All-Access Pass</p>
                             <p className="text-2xl font-text leading-[120%] mb-[24px]">Ultimate <br/> Access Pass</p>
                             <div className="w-full flex flex-col gap-[16px] mb-[32px]">
@@ -178,7 +181,7 @@ const Home = () => {
                                         </div>
                                         <Star className="w-[28px] h-[28px]" />
                                     </div>
-                                    <p className="leading-[160%]">Instant access to GPT-4, Claude, and other premium AI tools</p>
+                                    <p className="leading-[160%] text-xs">Instant access to GPT-4, Claude, and other premium AI tools</p>
                                 </div>
                                 <div className="w-full flex text-start gap-[16px]">
                                     <div className="relative w-[28px] h-[28px]">
@@ -186,7 +189,7 @@ const Home = () => {
                                         </div>
                                         <Star className="w-[28px] h-[28px]" />
                                     </div>
-                                    <p className="leading-[160%]">Sign in with ARES" across partner platforms (coming soon)</p>
+                                    <p className="leading-[160%] text-xs">Sign in with ARES" across partner platforms (coming soon)</p>
                                 </div>
                                 <div className="w-full flex text-start gap-[16px]">
                                     <div className="relative w-[28px] h-[28px]">
@@ -194,7 +197,7 @@ const Home = () => {
                                         </div>
                                         <Star className="w-[28px] h-[28px]" />
                                     </div>
-                                    <p className="leading-[160%]">One account, infinite possibilities</p>
+                                    <p className="leading-[160%] text-xs">One account, infinite possibilities</p>
                                 </div>
                             </div>
                             <div className="flex relative">
@@ -218,11 +221,168 @@ const Home = () => {
                             </div>
                         </div>
                         <div className="w-3/5 flex flex-col gap-[32px]">
-                            <div className="w-full h-1/2 border border-gold-light"></div>
-                            <div className="w-full h-1/2 border border-gold-light"></div>
+
+                            <div className="w-full h-1/2 bg-beige-light flex border border-gold-light p-[48px] gap-[32px]">
+
+                                <div className="flex flex-col">
+                                    <p className="text-gold text-xs mb-[12px] tracking-[1.4px] uppercase">Ridiculously Easy</p>
+                                    <p className="text-2xl font-text leading-[120%] mb-[24px]">So Easy, It Feels Illegal 
+                                        <br/>(It's Not)
+                                    </p>
+                                    <div className="w-full flex flex-col gap-[16px] mb-[32px]">
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">One unified dashboard</p>
+                                        </div>
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">Seamless switching between tools</p>
+                                        </div>
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">No more password juggling</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="flex-1"></div>
+
+                            </div>
+
+                            <div className="w-full h-1/2 bg-beige-light flex border border-gold-light p-[48px] gap-[32px]">
+
+                                <div className="flex-1"></div>
+
+                                <div className="flex flex-col">
+                                    <p className="text-gold text-xs mb-[12px] tracking-[1.4px] uppercase">Ridiculously Easy</p>
+                                    <p className="text-2xl font-text leading-[120%] mb-[24px]">Only Pay For Your Use,<br/>
+                                        Every Dollar Counts
+                                    </p>
+                                    <div className="w-full flex flex-col gap-[16px] mb-[32px]">
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">No wasted subscriptions</p>
+                                        </div>
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">Flexible credit system</p>
+                                        </div>
+                                        <div className="w-full flex text-start gap-[16px]">
+                                            <div className="relative w-[28px] h-[28px]">
+                                                <div className="absolute top-0 w-full h-full bg-gold bg-opacity-15 rounded-[980px] filter blur-24">
+                                                </div>
+                                                <Star className="w-[28px] h-[28px]" />
+                                            </div>
+                                            <p className="leading-[160%] text-xs">Real-time tracking</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
                         </div>
                     </div>
                     <p className="text-[18px]">We're constantly adding new partners to expand your access — all at no extra cost.</p>
+                </section>
+
+                <section className="w-full max-w-7xl flex flex-col justify-center items-center">
+
+                    <div className="w-full flex items-center justify-between mb-[80px]">
+                        <h2 className="text-4xl">Join 12,340 Tech Rebels</h2>
+                        <PrimaryButton text="Start Free Trial" />
+                    </div>
+
+                    <div className="w-full mb-[60px] flex gap-[32px] leading-[140%]">
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center text-center border border-gold-light">
+                            <div className="p-[48px]">
+                                <p>“Instead of paying $100s per month for software, I pay just $19 with ARES.”</p>
+                            </div>
+                            <div className="flex flex-col justify-center p-[32px] flex-1">
+                                <p>Rachita & John</p>
+                                <p>Shrub Finance</p>
+                            </div>
+                        </div>
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center text-center border border-gold-light">
+                            <div className="p-[48px]">
+                                <p>“Instead of paying $100s per month for software, I pay just $19 with ARES.”</p>
+                            </div>
+                            <div className="flex flex-col justify-center p-[32px] flex-1">
+                                <p>Daniela A.</p>
+                            </div>
+                        </div>
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center text-center border border-gold-light">
+                            <div className="p-[48px]">
+                                <p>“Instead of paying $100s per month for software, I pay just $19 with ARES.”</p>
+                            </div>
+                            <div className="flex flex-col justify-center p-[32px] flex-1">
+                                <p>Pat S.</p>
+                                <p>AccelB</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="w-full max-w-7xl flex flex-col justify-center items-center">
+
+                    <div className="w-full flex items-center justify-center mb-[80px]">
+                        <h2 className="text-4xl">Premium Tools, Ready to Roll</h2>
+                    </div>
+
+                    <div className="w-full mb-[60px] flex gap-[32px] leading-[140%]">
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center border border-gold-light">
+                            <div className="w-full h-[240px] bg-orange">
+                                
+                            </div>
+                            <div className="flex flex-col p-[48px] flex-1 gap-[16px]">
+                                <p className="font-text text-2xl">GPT-4o</p>
+                                <p>Chat with OpenAI’s most advanced AI model.</p>
+                            </div>
+                        </div>
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center border border-gold-light">
+                            <div className="w-full h-[240px] bg-orange">
+                                
+                            </div>
+                            <div className="flex flex-col p-[48px] flex-1 gap-[16px]">
+                                <p className="font-text text-2xl">Claude</p>
+                                <p>Generate complex discussions with ease.</p>
+                            </div>
+                        </div>
+
+                        <div className="w-1/3 bg-beige-light flex flex-col items-center border border-gold-light">
+                            <div className="w-full h-[240px] bg-orange">
+                                
+                            </div>
+                            <div className="flex flex-col p-[48px] flex-1 gap-[16px]">
+                                <p className="font-text text-2xl">Stable Diffusion</p>
+                                <p>Create breathtaking visual art.</p>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
